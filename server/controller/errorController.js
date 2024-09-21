89ddc5a9a3e247ca3c7b15ce480b0626e64299fb
Solomon Asregdew <117452698+SolomonAHailu@users.noreply.github.com>
2024-09-21 17:40:37 +0300

    Update errorController.js

--- server/controller/errorController.js
+++ server/controller/errorController.js
@@ -3,7 +3,7 @@ const sendErrorDev = (err, res) => {
 const statusCode = err.statusCode || 500;
 const status = err.status || "error";
 const message = err.message;
-  const stack = err.stack;
+const stack = err.stack;
 
   res.status(statusCode).json({
     status,
