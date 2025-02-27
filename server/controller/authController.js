3f6c46bf797965320aa404765b0a383fcd91ae9d
Solomon Asregdew <117452698+SolomonAHailu@users.noreply.github.com>
2025-02-27 08:48:57 +0300

    Update authController.js

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -10,7 +10,7 @@ const generateToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-}; 
+};
 
 const signup = catchAsync(async (req, res) => {
   const body = req.body;
